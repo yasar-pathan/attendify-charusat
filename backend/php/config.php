a8acccc8797d3aca26bf84a188c86e89556e5121
Yasar Pathan <d24it181@charusat.edu.in>
2026-06-18 21:57:54 +0530

    Update CORS whitelist to presentsir.tech

--- backend/php/config.php
+++ backend/php/config.php
@@ -14,8 +14,8 @@ $allowedOrigins = [
     'http://127.0.0.1:8081',
     'http://localhost:8080',
     'http://127.0.0.1:8080',
-    'https://present.tech',
-    'https://www.present.tech',
+    'https://presentsir.tech',
+    'https://www.presentsir.tech',
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
